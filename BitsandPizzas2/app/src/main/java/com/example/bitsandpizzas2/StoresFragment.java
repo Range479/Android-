e609c468f3e818a8fa65e84f60bf4b54aa8daf8c
Range479 <43882998+Range479@users.noreply.github.com>
2019-07-12 12:08:16 +0800

    设计支持库
    基于之前的“支持库和应用题”学习的设计支持库内容。

---
+++ BitsandPizzas2/app/src/main/java/com/example/bitsandpizzas2/StoresFragment.java
@@ -0,0 +1,23 @@
+package com.example.bitsandpizzas2;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class StoresFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                inflater.getContext(),
+                android.R.layout.simple_list_item_1,
+                getResources().getStringArray(R.array.stores));
+        setListAdapter(adapter);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+}
